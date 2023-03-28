91ae307e7a8775d26c1b44f9d6148e59138f887e
pratik <pratikdatey@gmail.com>
2023-03-28 10:59:09 +0530

    update

--- src/components/data_injection.py
+++ src/components/data_injection.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 
 @dataclass
@@ -21,7 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info('Entered the data ingestion method or component.')
         try:
-            df=pd.read_csv('notebook\data\StudentsPerformance.csv')
+            df=pd.read_csv('notebook\data\Stud.csv')
             logging.info('Data Reading Success')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -35,13 +37,18 @@ class DataIngestion:
             logging.info('Ingestion of data is completed')
             return (self.ingestion_config.train_data_path,
                     self.ingestion_config.test_data_path)
+        
+        
         except Exception as e:
             raise CustomException(e,sys)
         
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
         
 
 
