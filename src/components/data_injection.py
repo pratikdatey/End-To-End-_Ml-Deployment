aaf4eba67be3d9a0cb1f82b31e9b4e6afda40229
pratik <pratikdatey@gmail.com>
2023-04-30 09:45:48 +0530

    update

--- src/components/data_injection.py
+++ src/components/data_injection.py
@@ -45,10 +45,6 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
-        
+    obj.initiate_data_ingestion()
 
 
