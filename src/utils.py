8545a3e8e8da6ed971cd08d49af846a58364c98a
pratik <pratikdatey@gmail.com>
2023-05-03 09:31:34 +0530

    Flask

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import sys
 from src.exception import CustomException
 import dill
-
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -54,6 +54,15 @@ def evaluate_models(trainx, trainy,testx,testy,models,params):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
